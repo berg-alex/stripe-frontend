00c6e8e7210190ee1d351811e333631b65e58b23
Alex <abergsf2017@gmail.com>
2021-10-07 14:05:55 -0700

    test connect backend v4

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("https://stripe-one-time-pay.herokuapp.com/")
+    fetch("/https://stripe-one-time-pay.herokuapp.com/")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
